5f5b44c9422d43a000abebf1deee13ad17bfd60f
ArlynC <alejandra321.any@gmail.com>
2020-02-07 10:21:40 -0500

    read

--- app/src/main/java/com/example/micrud_upt/ReadProd.java
+++ app/src/main/java/com/example/micrud_upt/ReadProd.java
@@ -1,15 +1,14 @@
 package com.example.micrud_upt;
 
-import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,46 +17,38 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class ReadProd extends AppCompatActivity {
 
-    Retrofit retrofit;
-    servicesRetrofit miserviceretrofit;
+    Retrofit retrofit; servicesRetrofit miserviceretrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_read_prod);
         final String url = "http://productosuptarlyn.eu-4.evennode.com/";
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
+        Gson gson = new GsonBuilder().setLenient().create();
         retrofit = new Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
         miserviceretrofit = retrofit.create(servicesRetrofit.class);
-        checkserver();
+        listar();
     }
-    public void checkserver() {
-        Call<String> call = miserviceretrofit.getcheck();
-        call.enqueue(new Callback<String>() {
+    private void listar() {
+        Call<List<Productos>> call = miserviceretrofit.getlist();
+        call.enqueue(new Callback<List<Productos>>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
+            public void onResponse(Call<List<Productos>> call, Response<List<Productos>> response) {
                 Log.e("mirespuesta: ", response.toString());
-                Log.e("mirespuesta: ", response.body());
+                for(Productos res : response.body()) {
+                    Log.e("mirespuesta: ","id="+res.getId()+" prod="+res.getName() +" precio="+res.getPrice());
+                }
             }
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                Log.e("onFailure", t.toString());
+            public void onFailure(Call<List<Productos>> call, Throwable t) {
+                Log.e("onFailure", t.toString());// mostrmos el error
             }
         });
     }
 
-    public void create(View view) {
-        startActivity(new Intent(this,CreateProd.class));
-    }
-
-    public void read(View view) {
-        startActivity(new Intent(this, ReadProd.class));
-    }
 }
--- app/src/main/java/com/example/micrud_upt/servicesRetrofit.java
+++ app/src/main/java/com/example/micrud_upt/servicesRetrofit.java
@@ -1,5 +1,7 @@
 package com.example.micrud_upt;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -12,4 +14,7 @@ public interface servicesRetrofit {
 
     @POST("products/create")
     Call<String> newproducto(@Body Productos producto);
+
+    @GET("products/list")
+    Call<List<Productos>> getlist();
 }

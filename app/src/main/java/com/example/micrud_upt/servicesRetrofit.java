08bb50cab46374d1453b00a94ae4c8d9b452d916
ArlynC <alejandra321.any@gmail.com>
2020-02-07 10:16:49 -0500

    create

--- app/src/main/java/com/example/micrud_upt/servicesRetrofit.java
+++ app/src/main/java/com/example/micrud_upt/servicesRetrofit.java
@@ -1,11 +1,15 @@
 package com.example.micrud_upt;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface servicesRetrofit {
 
     @GET("products/test")
     Call<String> getcheck();
 
+    @POST("products/create")
+    Call<String> newproducto(@Body Productos producto);
 }
